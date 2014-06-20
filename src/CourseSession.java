926f7837ef54074d4df4342551bb84554ae12ca1
rent4yj <rent4yj@gmail.com>
2014-06-20 14:45:26 +0900

    2장 5절 하는중

--- src/CourseSession.java
+++ src/CourseSession.java
@@ -3,11 +3,13 @@ public class CourseSession {
 
 	private String department;
 	private String number;
+	private int numberOfStudents;
 
 	
 	public CourseSession(String department, String number) {
 		this.department = department;
 		this.number = number;		
+		numberOfStudents = 0;
 	}
 
 	public String getDepartment() {
@@ -16,6 +18,14 @@ public class CourseSession {
 
 	public String getNumber() {
 		return number;
+	}
+
+	public Object getNumberOfStudents() {
+		return numberOfStudents;
+	}
+	
+	void enroll(Student student){
+		numberOfStudents = numberOfStudents + 1; 
 	}
 
 }
